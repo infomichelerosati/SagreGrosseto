b8527a55bb34de0bf89a1753785f575bfcd76234
Michele Rosati <info.michele.rosati@gmail.com>
2026-07-02 15:14:01 +0200

    Rimossa sagra duplicata a Campagnatico

---
+++ remove_manual_dup.py
@@ -0,0 +1,12 @@
+﻿import csv
+with open("sagre.csv", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+
+# Filter out the "Sagra del Tortello & Bistecca" in Campagnatico (which is a duplicate of the longer event)
+new_lines = []
+for line in lines:
+    if "Sagra del Tortello & Bistecca,Campagnatico" not in line:
+        new_lines.append(line)
+
+with open("sagre.csv", "w", encoding="utf-8") as f:
+    f.writelines(new_lines)
